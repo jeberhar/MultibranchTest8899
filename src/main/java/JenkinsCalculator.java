7cdd0c055a44dd285932a20bd7be87d64e182d45
Jay E <jay.eberhard@gmail.com>
2022-02-10 10:21:45 -0700

    Add files via upload

---
+++ src/main/java/JenkinsCalculator.java
@@ -0,0 +1,15 @@
+package com.jenkins.jay;
+
+public class JenkinsCalculator{
+
+	//Method to add 2 numbers
+	public int addNumbers(int numberOne, int numberTwo){
+		return numberOne + numberTwo;
+	}
+
+	//Method to subtract 2 numbers
+	//
+	public int subtractNumbers(int numberOne, int numberTwo){
+                return numberOne - numberTwo;
+        }
+}
